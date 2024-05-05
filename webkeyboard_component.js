197c7f81da9b904dddb770eff3affd86c1eabb58
MajorX234 <majorx234@googlemail.com>
2024-05-05 23:58:01 +0200

    Add MidiRest Class and event handling of keyoard

--- webkeyboard_component.js
+++ webkeyboard_component.js
@@ -118,18 +118,97 @@ $.fn.bindMobileEvents = function () {
       );
 
       first.target.dispatchEvent(simulatedEvent);
-    });
-  };
+  });
+};
 
+function getServerAddress() {
+  var server_adress = 'http://'.concat(location.hostname,':8000');
+  return server_adress;
+}
+
+class MidiREST {
+  constructor(){
+  }
+
+  note_on(key){
+    var midi_msg = [0x91, key, 63];
+    this.send_midimsgs(midi_msg);
+  }
+
+  note_off(key){
+    var midi_msg = [0x81, key, 63];
+    this.send_midimsgs(midi_msg);
+  }
+
+  send_midimsgs(midi_data){
+    let send_raw = true;
+    let midi_msg = midi_data;
+    if (!send_raw) {
+      midi_msg = {
+        status:send_raw[0], data1:send_raw[0], data2:send_raw[0]
+      };
+    }
+    this.send_data(JSON.stringify(midi_msg));
+  }
+
+    send_data(midi_msg, debug){
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        alert(this.responseText);
+      }
+    };
+    let midi_endpoint = getServerAddress() + '/midi_endpoint';
+    xhttp.open("POST", midi_endpoint, true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(midi_msg);
+    // TODO fix console
+    // outputToConsole("send: " + midi_msg);
+  }
+}
 
 class WebkeyboardComponent extends HTMLElement {
     constructor() {
         super();
         this.root = this.attachShadow({mode: "closed"});
         this.root.appendChild(template.content.cloneNode(true));
+        this.current_index = 0;
+        this.clicked = false;
+        this.midi = new MidiREST();
+
     }
     connectedCallback() {
         this.root.querySelectorAll(".key").bindMobileEvents();
+        this.root.querySelectorAll(".key").on("mousedown", function() {
+            var index = 60 + $(this).index('.key');
+            midi.note_on(index);
+            current_index = index;
+            clicked = true;
+        });
+
+        this.root.querySelectorAll(".key").on("mouseleave", function() {
+            var index = 60 + $(this).index('.key');
+            if(clicked == true){
+                midi.note_off(index);
+            }
+        });
+
+        this.root.querySelectorAll(".key").on("mouseover",function() {
+            var index = 60 + $(this).index('.key');
+            if(clicked == true){
+                midi.note_on(index);
+            }
+        });
+
+        this.root.querySelectorAll(".piano").on("mouseleave", function() {
+            clicked = false;
+        });
+
+        this.root.querySelectorAll(".key").on("mouseup", function() {
+            var index = 60 + $(this).index('.key');
+            midi.note_off(index);
+            clicked = false;
+        });
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
